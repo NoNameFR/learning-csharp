3ed1216a61264b3df1fefc5e6050cb42df9d0c49
NoNameFR <guipasmoi@hotmail.com>
2020-08-11 17:57:50 +0200

    Indentation

--- Program.cs
+++ Program.cs
@@ -12,33 +12,31 @@ namespace HelloWorld
             var mamie = new Person { Lastname = "Christine", Firstname = "SEVAUX", Age = 30 };
             var kemen = new Person { Lastname = "Kemen", Firstname = "DAGUERRE", Age = 40 };
             var laetitia = new Person { Lastname = "Laetitia", Firstname = "DAGUERRE", Age = 50 };
-           // var newPerson = Person.CreatePerson();
+            
             var average = 0;
             var numberOfPerson = 0;
             List<Person> famille = new List<Person>() { dorian, guillaume, mamie, kemen, laetitia};
-
-         
+            
             foreach (Person person in famille)
             {
                 Console.WriteLine($"{person.Lastname} {person.Firstname} is {person.Age} years old.");
             }
+            
             foreach (Person person in famille)
             {
                 average = average + person.Age;
                 numberOfPerson = numberOfPerson + 1;
-                
             }
             average = average / numberOfPerson;
             double standardDeviation = 0;
+            
             foreach (Person person in famille)
             {
-             
                 standardDeviation= standardDeviation + Math.Pow(person.Age - average, 2);
             }
             standardDeviation= Math.Sqrt(standardDeviation / numberOfPerson);
             Console.WriteLine("Average age is " + average + ".");
             Console.WriteLine("Standard deviation is " + standardDeviation);
-
         }
     }
 }
